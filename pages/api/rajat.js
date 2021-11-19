9aad6249b6140b85b53ac5098944476a309a60db
hyvip-ai <rm2932002@gmail.com>
2021-11-19 16:53:35 +0530

    added lang for html

---
+++ pages/api/rajat.js
@@ -0,0 +1,5 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({ name: 'Rajat Mondal', developed_by:"Rajat Mondal", app_name:"World Rank", version:'v.0.0.1',running:"successfully" })
+}
